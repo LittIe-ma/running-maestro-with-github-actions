ac270b27ea4aabb6b0aa03d556fdd60c50ed46c6
yasudamasato <yasudaM84@gmail.com>
2025-01-12 14:21:39 +0900

    Initial Commit

---
+++ SampleMaestro/SampleMaestroApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleMaestroApp.swift
+//  SampleMaestro
+//
+//  Created by Masato Yasuda on 2025/01/12.
+//
+
+import SwiftUI
+
+@main
+struct SampleMaestroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
